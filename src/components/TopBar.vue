9895826f5beaba7948035931c96edd240638ebe6
Honohonoho <751718620@qq.com>
2017-06-23 15:34:15 +0800

    完善TopBar.vue功能，添加动画效果

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -12,8 +12,18 @@
         <a href="#">登录</a>
         <a href="#">注册</a>
       </div>
-      <div class="topbar-cart">
-        <a href="#">购物车（<span>0</span>）</a>
+      <div class="topbar-cart"
+        @mouseenter="cartEnter"
+        @mouseleave="cartOut">
+        <div class="cart" :class="{'active': cartStatus}">
+          <span class="icon"></span>
+          <a href="#">购物车（<span>0</span>）</a>
+        </div>
+        <transition name="expand">
+          <div class="cart-detail" v-show="cartStatus">
+            购物车中还没有商品，赶快选购吧！
+          </div>
+        </transition>
       </div>
     </div>
   </div>
@@ -34,11 +44,22 @@ export default {
         {'name': '小米网移动版'},
         {'name': '问题反馈'},
         {'name': 'Select Region'},
-      ]
+      ],
+      timer: '',
+      cartStatus: false
     }
   },
   methods: {
-
+    cartEnter: function(){
+      this.cartStatus = true
+      clearInterval(this.timer)
+    },
+    cartOut: function(){
+      var self = this
+      this.timer = setTimeout(()=>{
+        self.cartStatus = false
+      }, 200)
+    }
   },
   components: {
 
@@ -52,10 +73,11 @@ export default {
   background: #333;
   >.top-bar{
     width: 1226px;
+    background: #333;
     height: 40px;
     margin: 0 auto;
     display: flex;
-    justify-content: flex-start;
+    justify-content: space-between;
     align-items: center;
     >.topbar-nav{
       >ul{
@@ -79,7 +101,7 @@ export default {
       }
     }
     >.topbar-info{
-      margin-left: 500px;
+      margin-left: auto;
       >a{
         color: #b0b0b0;
         padding: 0 .5em;
@@ -93,19 +115,57 @@ export default {
       }
     }
     >.topbar-cart{
-      >a{
-        color: #b0b0b0;
-        padding: 0 .5em;
-        border-right: 1px solid #424242;
-        &:last-child{
-        border: none;
+      position: relative;
+      width: 120px;
+      line-height: 40px;
+      text-align: center;
+      background: #424242;
+      cursor: pointer;
+      display: flex;
+      flex-direction: column;
+      >.cart{
+        a{
+          color: #b0b0b0;
+          padding: 0 .5em;
+          border-right: 1px solid #424242;
+          &:last-child{
+          border: none;
+          }
         }
-        &:hover{
-          color: #fff;
+      }
+      >.active{
+        a{
+          color: #ff6700;
         }
+        .icon{
+          color: #ff6700;
+        }
+      }
+      >.cart-detail{
+        position: absolute;
+        left: 120px;
+        transform: translate(-100%,0);
+        top: 40px;
+        width: 316px;
+        line-height: 96px;
+        padding: 0 15px;
+        color: #424242;
+        box-shadow: 0 2px 10px rgba(0,0,0,0.15);
       }
     }
   }
+}
+//购物车动画
+
+.expand-enter-active, .expand-leave-active {
+  transition: all .3s ease;
+  height: 96px;
+  opacity: 1;
+}
+.expand-enter,.expand-leave-to/* .fade-leave-active in <2.1.8 */ {
+  height: 0;
+  opacity: 0;
+  transition: all .4s ease;
 }
 </style>
 
