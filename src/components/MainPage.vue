8a0df2edaedf918c6f178ece5d606d5940eb6cb3
Honohonoho <751718620@qq.com>
2017-06-27 10:45:58 +0800

    完成 .main-page 框架

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -5,7 +5,28 @@
       <mainCarousel></mainCarousel>
       <mainRecommend></mainRecommend>
     </div>
-    
+    <div class="main-page">
+      <!--小米明星单品-->
+      <starGoods></starGoods>
+      <!--家电-->
+      <householdAppliances></householdAppliances>
+      <!--智能-->
+      <intelligence></intelligence>
+      <!--搭配-->
+      <collocation></collocation>
+      <!--配件-->
+      <parts></parts>
+      <!--周边-->
+      <relatedProducts></relatedProducts>
+      <!--为你推荐-->
+      <recommend></recommend>
+      <!--热评产品-->
+      <hotGoods></hotGoods>
+      <!--内容-->
+      <content></content>
+      <!--视频-->
+      <video></video>
+    </div>
   </div>
 </template>
 
@@ -13,12 +34,32 @@
 import MainNav from './MainNav'
 import MainCarousel from './MainCarousel'
 import MainRecommend from './MainRecommend'
+import StarGoods from './StarGoods'
+import HouseholdAppliances from './HouseholdAppliances'
+import Intelligence from './Intelligence'
+import Collocation from './Collocation'
+import Parts from './Parts'
+import RelatedProducts from './RelatedProducts'
+import Recommend from './Recommend'
+import HotGoods from './HotGoods'
+import Content from './Content'
+import Video from './Video'
 
 export default {
   components: {
     'mainNav': MainNav,
     'mainCarousel': MainCarousel,
-    'mainRecommend': MainRecommend
+    'mainRecommend': MainRecommend,
+    'starGoods': StarGoods,
+    'householdAppliances': HouseholdAppliances,
+    'intelligence': Intelligence,
+    'collocation': Collocation,
+    'parts': Parts,
+    'relatedProducts': RelatedProducts,
+    'recommend': Recommend,
+    'hotGoods': HotGoods,
+    'content': Content,
+    'video': Video
   }
 }
 </script>
